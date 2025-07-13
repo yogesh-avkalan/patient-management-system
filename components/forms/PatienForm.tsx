6fb72743e6df47b27116bc576a0423bcc756fd94
yogesh-avkalan <yogesh.avkalan@gmail.com>
2025-07-13 13:33:27 +0530

    refactor: restructure form components and update styles in globals.css

--- components/forms/PatienForm.tsx
+++ components/forms/PatienForm.tsx
@@ -1,29 +1,32 @@
 "use client"
-
+ 
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
-import { Button } from "@/components/ui/button"
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-
-
+ 
 const formSchema = z.object({
   username: z.string().min(2, {
     message: "Username must be at least 2 characters.",
   }),
 })
 
-const PatienForms = () => {
- <Form {...form}>
+const PatienForm = () => {
+    // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  })
+ 
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // This will be type-safe and validated.
+    console.log(values)
+  }
+  return (
+     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
         <FormField
           control={form.control}
@@ -44,6 +47,7 @@ const PatienForms = () => {
         <Button type="submit">Submit</Button>
       </form>
     </Form>
+  )
 }
 
-export default PatienForms
+export default PatienForm
